6344d3e41f5b87687f82c9db1bf795e1bd25072e
CaiusBelcescu <80705095+CaiusBelcescu@users.noreply.github.com>
2022-11-09 23:14:37 +0200

    Implementation with an error on the deleting part

---
+++ Chat/src/Server.java
@@ -0,0 +1,30 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+    private ServerSocket serverSocket;
+    public Server(ServerSocket serverSocket){
+        this.serverSocket=serverSocket;
+    }
+    public void startServer(){
+        try{
+            while(!serverSocket.isClosed()){
+                Socket socket= serverSocket.accept();
+                System.out.println("A client has connected!");
+                ClientHandler clientHandler = new ClientHandler(socket);
+
+                Thread thread=new Thread(clientHandler);
+                thread.start();
+            }
+        }catch(IOException e){
+
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket =new ServerSocket(1234);
+        Server server = new Server(serverSocket);
+        server.startServer();
+    }
+}
