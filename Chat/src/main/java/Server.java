490464fe54f299f349677eff21f6471919ac5343
Ioftor Victor <vioftor@gmail.com>
2022-12-13 21:28:06 +0200

    integrated maven

--- Chat/src/main/java/Server.java
+++ Chat/src/main/java/Server.java
@@ -2,6 +2,7 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+
 public class Server {
     private ServerSocket serverSocket;
     public Server(ServerSocket serverSocket){
