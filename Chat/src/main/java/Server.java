05420dbed1ec1e96bee7eed3d47b586624e2d69f
Ioftor Victor <vioftor@gmail.com>
2022-12-20 23:12:56 +0200

    changes

--- Chat/src/main/java/Server.java
+++ Chat/src/main/java/Server.java
@@ -19,38 +19,35 @@ public class Server extends Thread{
                 verifyUsersConnectivity();
             } catch (IOException e) {}
         });
-
-        //every 5 seconds verify user connectivity
         serverScheduler.scheduleAtFixedRate(thread, 0, 5000, TimeUnit.MILLISECONDS);
     }
 
     //checks the timestamp of all users
     public void verifyUsersConnectivity() throws IOException{
-        List<String> clients = new ArrayList(clientList.keySet());
-
-        for(String client: clients){
-            Iterator<Map.Entry<String, Long>> iterator = clientList.entrySet().iterator();
-            while (iterator.hasNext()){
-                Map.Entry<String, Long> entry = iterator.next();
-                if(client.equals(entry.getKey())) {
-                    long idleTime = (-1 * (entry.getValue() - serverTimestamp.getTime()));
-                    if(idleTime > 5100){
-                        System.out.println("Client " + client + " idle for: " + idleTime/1000 + " seconds, removing");
-                        clientList.remove(client);
-                        iterator.remove();
-                    }
-                    else {
-                        System.out.println("Client " + client + " idle for: " + idleTime/1000 + " seconds, updating timeStamp");
-                    }
-                }
-            }
-        }
+//        List<String> clients = new ArrayList(clientList.keySet());
+//
+//        for(String client: clients){
+//            Iterator<Map.Entry<String, Long>> iterator = clientList.entrySet().iterator();
+//            while (iterator.hasNext()){
+//                Map.Entry<String, Long> entry = iterator.next();
+//                if(client.equals(entry.getKey())) {
+//                    long idleTime = (-1 * (entry.getValue() - serverTimestamp.getTime()));
+//                    if(idleTime > 5100){
+//                        System.out.println("Client " + client + " idle for: " + idleTime/1000 + " seconds");
+//                        clientList.remove(client);
+//                        iterator.remove();
+//                    }
+//                    else {
+//                        System.out.println("Client " + client + " idle for: " + idleTime/1000 + " seconds");
+//                    }
+//                }
+//            }
+//        }
     }
 
     //add user to clientList
     public static void addUser(String username) {
         List<String> clients = new ArrayList(clientList.keySet());
-
         if(!clients.contains(username))
         {
             clientList.put(username, serverTimestamp.getTime());
@@ -61,28 +58,17 @@ public class Server extends Thread{
     }
 
     //returns true if user is connected
-    public static boolean isUserConnected(String user){
+    public static boolean userConnected(String user){
         List<String> clients = new ArrayList(clientList.keySet());
-
         if(clients.contains(user)) {
             return true;
         }
-
         return false;
-    }
-
-    //shows all online users
-    public static void showOnlineUsers(){
-        List<String> clients = new ArrayList(clientList.keySet());
-
-        System.out.println(clients);
-
     }
 
     //returns true if user is still in the list and sets his timestamp
     public static boolean setUserTimestamp(String user){
         Iterator<Map.Entry<String, Long>> iterator = clientList.entrySet().iterator();
-
         while (iterator.hasNext()){
             Map.Entry<String, Long> entry = iterator.next();
             if(user.equals(entry.getKey())){
@@ -90,13 +76,11 @@ public class Server extends Thread{
                 return true;
             }
         }
-
         return false;
     }
 
     //creates server queue
     public static void task() throws IOException, TimeoutException {
-
         ConnectionFactory connectionFactory = new ConnectionFactory();
         connectionFactory.setHost("localhost");
 
@@ -104,20 +88,16 @@ public class Server extends Thread{
         Channel serverChannel = serverConnection.createChannel();
         serverChannel.queueDeclare("server", false, false, false, null);
         serverChannel.queuePurge("server"); //clear all messages from queue
-
-        serverChannel.basicQos(1);
+        serverChannel.basicQos(1);//receives only 1 unacknowledged message at once
 
         DeliverCallback serverCallback = (consumer, delivery) -> {
-
             AMQP.BasicProperties replyProprieties = new AMQP.BasicProperties
                     .Builder()
                     .correlationId(delivery.getProperties().getCorrelationId())
                     .build();
 
             String response = "";
-
             String[] request = new String(delivery.getBody(), StandardCharsets.UTF_8).split("->");
-
             switch(request[0]){
                 case "addUser":
                     addUser(request[1]);
@@ -126,17 +106,15 @@ public class Server extends Thread{
                     setUserTimestamp(request[1]);
                     break;
                 case "CONNECTED?":
-                    if(isUserConnected(request[1]))
+                    if(userConnected(request[1]))
                         response = "true";
                     else response = "false";
                     break;
                 default:
             }
-
             serverChannel.basicPublish("", delivery.getProperties().getReplyTo(), replyProprieties, response.getBytes(StandardCharsets.UTF_8));
             serverChannel.basicAck(delivery.getEnvelope().getDeliveryTag(), false);
         };
-
         serverChannel.basicConsume("server",  false, serverCallback, (CONSUMER -> {}));
 
 
